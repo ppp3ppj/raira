218a6cf22b75250f254ab82ce40fb35051f3455c
ppp3ppj <ppp.3ppj@gmail.com>
2025-09-03 19:22:49 +0700

    Generate auth system using argon2 hashing password

--- lib/raira_web/router.ex
+++ lib/raira_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RairaWeb.Router do
   use RairaWeb, :router
 
+  import RairaWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RairaWeb.Router do
     plug :put_root_layout, html: {RairaWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule RairaWeb.Router do
       live_dashboard "/dashboard", metrics: RairaWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", RairaWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{RairaWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", RairaWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{RairaWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
