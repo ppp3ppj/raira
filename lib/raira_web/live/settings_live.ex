705688511c4221d420193a1c206970726ebec128
ppp3ppj <ppp.3ppj@gmail.com>
2025-10-02 16:00:04 +0700

    Apply title component to home and setting

--- lib/raira_web/live/settings_live.ex
+++ lib/raira_web/live/settings_live.ex
@@ -11,7 +11,20 @@ defmodule RairaWeb.SettingsLive do
   def render(assigns) do
     ~H"""
     <LayoutComponents.layout current_page={~p"/settings"}>
-      <p>Settings</p>
+      <div class="p-4 md:px-12 md:py-6 max-w-screen-lg mx-auto">
+        <div class="flex flex-row space-y-0 items-center pb-4 justify-between">
+          <LayoutComponents.title text="Settings" />
+          <div class="hidden md:flex space-x-2" role="navigation" aria-label="new notebook">
+            <.button color="gray" outlined navigate={~p"/open/storage"}>
+              Open
+            </.button>
+            <.button color="blue" patch={~p"/new"}>
+              <.icon name="hero-add-line" />
+              <span>New notebook</span>
+            </.button>
+          </div>
+        </div>
+      </div>
     </LayoutComponents.layout>
     """
   end
