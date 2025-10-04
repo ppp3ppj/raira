7b0701b4aa48ffe0faa2c56bc0b23a198a44e43a
ppp3ppj <ppp.3ppj@gmail.com>
2025-10-04 11:01:50 +0700

    Add name and email in popup user profile

--- lib/raira_web/live/user_component.ex
+++ lib/raira_web/live/user_component.ex
@@ -7,9 +7,10 @@ defmodule RairaWeb.UserComponent do
     socket = assign(socket, assigns)
     user = socket.assigns.user
     # changeset = Users.change_user(user)
+    changeset = Raira.Accounts.change_user(user)
 
-    # {:ok, assign(socket, changeset: changeset, user: user)}
-    {:ok, assign(socket, user: user)}
+    {:ok, assign(socket, changeset: changeset, user: user)}
+    # {:ok, assign(socket, user: user)}
   end
 
   @impl true
@@ -22,7 +23,50 @@ defmodule RairaWeb.UserComponent do
       <div class="flex justify-center">
         <.user_avatar user={@user} class="h-20 w-20" text_class="text-3xl" />
       </div>
+      <.form
+        :let={f}
+        for={@changeset}
+        phx-submit={@on_save |> JS.push("save")}
+        phx-change="validate"
+        phx-target={@myself}
+        id="user_form"
+        phx-hook="UserForm"
+      >
+        <!--
+        <div class="flex flex-col space-y-5">
+          <.text_field
+            field={f[:name]}
+            label="Display name"
+            spellcheck="false"
+            disabled={Livebook.Config.identity_provider_read_only?()}
+          />
+        -->
 
+        <div class="flex flex-col space-y-5">
+          <.text_field
+            field={f[:name]}
+            label="Display name"
+            spellcheck="false"
+          />
+          <%= if @user.email do %>
+            <.text_field field={f[:email]} label="email" spellcheck="false" disabled="true" />
+          <% end %>
+          <!--
+          <.hex_color_field
+            field={f[:hex_color]}
+            label="Cursor color"
+            randomize={JS.push("randomize_color", target: @myself)}
+          />
+          -->
+          <.button type="submit" disabled={not @changeset.valid?}>
+            <.icon name="hero-archive-box-arrow-down" />
+            <!--
+            <.remix_icon icon="save-line" />
+            -->
+            <span>Save</span>
+          </.button>
+        </div>
+      </.form>
     </div>
     """
   end
