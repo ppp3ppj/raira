2b05750aa31ea79f19056b27fa99807fe0b9fd1e
ppp3ppj <ppp.3ppj@gmail.com>
2025-11-07 14:24:23 +0700

    Update app layout to drawer layout

--- lib/raira_web/live/project_live/show.ex
+++ lib/raira_web/live/project_live/show.ex
@@ -1,31 +1,37 @@
 defmodule RairaWeb.ProjectLive.Show do
   use RairaWeb, :live_view
 
+  alias RairaWeb.LayoutComponents
   alias Raira.Projects
 
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash} current_scope={@current_scope}>
-      <.header>
-        Project {@project.id}
-        <:subtitle>This is a project record from your database.</:subtitle>
-        <:actions>
-          <.button navigate={~p"/projects"}>
-            <.icon name="ri-arrow-go-back-line" class="h-4 w-4" />
-          </.button>
-          <.button variant="primary" navigate={~p"/projects/#{@project}/edit?return_to=show"}>
-            <.icon name="ri-edit-line" class="h-4 w-4" /> Edit project
-          </.button>
-        </:actions>
-      </.header>
+    <LayoutComponents.drawer_layout current_page={~p"/"} current_user={@current_scope.user}>
+      <div class="relative w-full max-w-screen-lg px-4 sm:pl-8 sm:pr-16 md:pl-16 pt-4 sm:py-5 mx-auto">
+        <div class="flex flex-row space-y-0 items-center pb-4 justify-between">
+          <LayoutComponents.title text="Project" />
+        </div>
+        <.header>
+          Project {@project.id}
+          <:subtitle>This is a project record from your database.</:subtitle>
+          <:actions>
+            <.button navigate={~p"/projects"}>
+              <.icon name="ri-arrow-go-back-line" class="h-4 w-4" />
+            </.button>
+            <.button variant="primary" navigate={~p"/projects/#{@project}/edit?return_to=show"}>
+              <.icon name="ri-edit-line" class="h-4 w-4" /> Edit project
+            </.button>
+          </:actions>
+        </.header>
 
-      <.list>
-        <:item title="Name">{@project.name}</:item>
-        <:item title="Description">{@project.description}</:item>
-        <:item title="Status">{@project.status}</:item>
-      </.list>
-    </Layouts.app>
+        <.list>
+          <:item title="Name">{@project.name}</:item>
+          <:item title="Description">{@project.description}</:item>
+          <:item title="Status">{@project.status}</:item>
+        </.list>
+      </div>
+    </LayoutComponents.drawer_layout>
     """
   end
 
